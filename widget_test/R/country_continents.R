2124632bb076e9d28ea963b67e9310f119904eea
hamishgibbs <hamishgibs@gmail.com>
2020-04-26 12:55:19 +0100

    continent reference

--- widget_test/R/country_continents.R
+++ widget_test/R/country_continents.R
@@ -4,7 +4,7 @@ cont <- read_csv('https://pkgstore.datahub.io/JohnSnowLabs/country-and-continent
 
 c
 cont
-c %>% 
+c_ref <- c %>% 
   left_join(cont, by = c('iso_a3' = 'Three_Letter_Country_Code')) %>% 
   st_set_geometry(NULL) %>% 
   dplyr::select(sovereignt,  iso_a3, Continent_Name) %>% 
@@ -12,5 +12,15 @@ c %>%
   rename(continent = Continent_Name) %>% 
   mutate(continent = ifelse(continent == 'North America', 'Americas', continent),
          continent = ifelse(continent == 'South America', 'Americas', continent)) %>% 
-   filter(continent != 'Antarctica') %>% 
-  write_csv('../../continent_reference.csv')
+   filter(continent != 'Antarctica')
+
+rt <- read_csv('https://raw.githubusercontent.com/hamishgibbs/rt_interactive_vis/master/rt.csv')
+
+
+rt_c <- rt %>% 
+  mutate(iso = countrycode(country, 'country.name', 'iso3c')) %>% 
+  left_join(c_ref, by = c('iso' = 'iso_a3')) %>% 
+  dplyr::select(-sovereignt) %>% 
+  mutate(continent = ifelse(country %in% c('Bahrain', 'Singapore'), 'Asia', continent))
+
+write_csv(rt_c, '../../rt_cont.csv')
